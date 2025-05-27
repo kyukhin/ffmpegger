ad6aae34b7f521f94d99c4994cb3f4de5a0645d4
Kirill Yukhin <kyukhin@tarantool.org>
2025-05-27 15:29:38 +0300

    Add more garbage words to list in names cleanup

--- cleanup-names.sh
+++ cleanup-names.sh
@@ -10,13 +10,21 @@ find "$DIR" -type f \( -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" \) |wh
     base=$(basename "$file")
 
     temp_name="$base"
-    if [[ "$temp_name" =~ ^(.+?)[._-]?(WEB|BD)[-_.]?(DL|Rip).*\.([a-zA-Z0-9]+)$ ]]; then
+    if [[ "$temp_name" =~ ^(.+?)[._-]?(WEB|BD)[-_.]?(DL|Rip|rip).*\.([a-zA-Z0-9]+)$ ]]; then
         temp_name="${BASH_REMATCH[1]}.${BASH_REMATCH[4]}"
     fi
 
     if [[ "$temp_name" =~ ^(.+?)[._-]?(720|1080)[pP].*\.([a-zA-Z0-9]+)$ ]]; then
         temp_name="${BASH_REMATCH[1]}.${BASH_REMATCH[3]}"
     fi
+
+    # Random words
+    if [[ "$temp_name" =~ ^(.+?)[._-]?(dfcbit|Rus|Video).*\.([a-zA-Z0-9]+)$ ]]; then
+        temp_name="${BASH_REMATCH[1]}.${BASH_REMATCH[3]}"
+    fi
+
+    # Remove spaces from filename
+    temp_name="${temp_name// /.}"
 
     clean_name=$(echo "$temp_name" | sed -E 's/[._-]+\.([a-zA-Z0-9]+)$/.\1/')
     new_path="$dir/$clean_name"
